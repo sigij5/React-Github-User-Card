26baa86fae59b7c44445a9b53abeb9dd23b92d2c
Sigi Jaeckel <sigij55@gmail.com>
2020-07-07 16:38:13 -0400

    rendering api data to app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import User from './components/User'
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userInfo: {},
+      following: []
+    }
+    console.log('constructor is running')
+  }
+
+  componentDidMount() {
+    console.log('cdm is running')
+    axios.get('https://api.github.com/users/sigij5')
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        userInfo: res.data
+      })
+    })
+  }
+
+  fetchFollowing = e => {
+    e.preventDefault()
+    axios.get(`https://api.github.com/users/${this.state.userInfo.login}/following`)
+    .then(res => {
+        this.setState({
+            following: res.data
+        })
+    })
+  }
+
+  render() {
+    console.log('rendering')
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Github User Cards</h1>
       </header>
+
+      <User info={this.state.userInfo} />
+
+      <div className='following'>
+      <h3 onClick={this.fetchFollowing}>Following</h3>
+
+      {this.state.following.map(user => 
+        <User info={user} />
+        )}
+      </div>
+
+      
+
+      {/* {this.state.users.map(user => <div>{user.username}</div>)} */}
     </div>
   );
-}
+  }
+} 
 
 export default App;
